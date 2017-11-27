afa5b89901244fead94f15eba9b23ce742589c61
drmilde <milde@hs-fulda.de>
2017-11-27 08:26:41 +0100

    Preferencescreen angefangen Timer implementiert Modale Knöpfe realisiert

--- app/src/main/java/com/example/milde/antoniaseinmaleins/MainActivity.java
+++ app/src/main/java/com/example/milde/antoniaseinmaleins/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -33,5 +35,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/milde/antoniaseinmaleins/SettingsActivity.java
+++ app/src/main/java/com/example/milde/antoniaseinmaleins/SettingsActivity.java
@@ -1,13 +1,14 @@
 package com.example.milde.antoniaseinmaleins;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        //setContentView(R.layout.activity_settings);
+        addPreferencesFromResource(R.xml.preferences);
     }
 }
