52b96d8006e959ead6f182527b68ec1101a33e85
drmilde <milde@hs-fulda.de>
2017-12-07 18:59:57 +0100

    UI Eigenschaften der SettingsActiviy (Preferences) angepasst

--- app/src/main/java/com/example/milde/antoniaseinmaleins/SettingsActivity.java
+++ app/src/main/java/com/example/milde/antoniaseinmaleins/SettingsActivity.java
@@ -2,12 +2,22 @@ package com.example.milde.antoniaseinmaleins;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.util.Log;
 
-public class SettingsActivity extends PreferenceActivity {
+import java.util.prefs.PreferenceChangeEvent;
+import java.util.prefs.PreferenceChangeListener;
+
+public class SettingsActivity extends PreferenceActivity implements PreferenceChangeListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    // KLAPPT NICHT ... muss nachgucken
+    @Override
+    public void preferenceChange(PreferenceChangeEvent preferenceChangeEvent) {
+        Log.d("Jan", "Preferences modified");
     }
 }
