5df80f74cf35e22a7be7180a913c59d0a127021b
drmilde <milde@hs-fulda.de>
2017-12-08 16:44:25 +0100

    ReihenauswahlActivity angefrangen Farben stimmen noch nicht

---
+++ app/src/main/java/com/example/milde/antoniaseinmaleins/Logik/ReihenAuswahlActivity.java
@@ -0,0 +1,15 @@
+package com.example.milde.antoniaseinmaleins.Logik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.milde.antoniaseinmaleins.R;
+
+public class ReihenAuswahlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reihen_auswahl);
+    }
+}
